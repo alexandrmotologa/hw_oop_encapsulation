d6494416565df5cf2c022db5b2861f2531d2f1c5
alexandrmotologa <al.motologa@gmail.com>
2020-08-18 17:42:01 +0300

    hw1 - 3

--- hw_oop_encapsulation.py
+++ hw_oop_encapsulation.py
@@ -20,19 +20,32 @@ class Weather():
     def __gt__(self, value):
         return self.temperature['avgTemp'] > value.temperature['avgTemp']
     
+    def __lt__(self, value):
+        return self.temperature['avgTemp'] < value.temperature['avgTemp']
+    
     def __eq__(self, value):
         return self.temperature['avgTemp'] == value.temperature['avgTemp'] and self.sky == value.sky
+    
+    def __ne__(self, value):
+        return self.temperature['avgTemp'] != value.temperature['avgTemp'] or self.sky != value.sky
 
 ###### CONSUMED
 
 todays_wheather = Weather(17, 8, 2020, 35, 35, 'clear')
-tomorrows_weather = Weather(18, 8, 2020, 25, 35, 'clear')
+tomorrows_weather = Weather(18, 8, 2020, 20, 10, 'clear')
 
 print(todays_wheather)
 print(tomorrows_weather)
 
+if todays_wheather != tomorrows_weather:
+    print(f"The weather today differs from tomorrow!")
+
 if todays_wheather > tomorrows_weather:
     print("Today is hotter than it will be TOMORROW!")
 
+if todays_wheather < tomorrows_weather:
+    print("Tomorrow is warmer than today!")
+
 if todays_wheather == tomorrows_weather:
     print("The weather tomorrow will be like TODAYS!!")
+
